b046c0327b5310b47bcafef6f3cfddd2fb83e253
Josh <54466340+Josham85@users.noreply.github.com>
2025-07-26 11:25:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -133,10 +133,8 @@ Input: {user_input}
     return render_template("result.html", output=result)
 
 
-#if __name__ == "__main__":
- #   import os
-#port = int(os.environ.get("PORT", 5000))
-#app.run(host="0.0.0.0", port=port)
-
 if __name__ == "__main__":
-    app.run(debug=True)
+  import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
